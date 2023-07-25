499e6ad8f97d2e4e0c9390e8ed7844475982777f
Abhiram K <115892203+Abhi-ram-K@users.noreply.github.com>
2023-07-25 18:13:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,16 @@
 // create web server 
-// 1. create web server
-// 2. create router
-// 3. create route
-// 4. start server
+// 1. npm install express
+// 2. npm install body-parser
+// 3. npm install cors
+// 4. npm install --save-dev nodemon
+// 5. npm install mongoose
+// 6. npm install dotenv
 
-// 1. create web server
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
 const app = express();
-const port = 3000;
-
-// 2. create router
-const router = express.Router();
-
-// 3. create route
-router.get('/list', (req, res) => {
-    res.send('comments list');
-});
-
-router.get('/delete', (req, res) => {
-    res.send('comments delete');
-});
-
-router.get('/update', (req, res) => {
-    res.send('comments update');
-});
-
-router.get('/insert', (req, res) => {
-    res.send('comments insert');
-});
-
-// 4. start server
-app.use('/comments', router);
-app.listen(port, () => {
-    console.log(`server start at http://localhost:${port}`);
-});
+dotenv
